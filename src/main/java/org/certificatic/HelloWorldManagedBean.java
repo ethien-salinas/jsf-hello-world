9f5f782caad4f2e8e849a443ed993da1dfb1da92
Ethien Salinas <ethien.salinas@gmail.com>
2017-10-12 08:12:02 -0500

    first commit

---
+++ src/main/java/org/certificatic/HelloWorldManagedBean.java
@@ -0,0 +1,22 @@
+package org.certificatic;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="helloBean")
+@SessionScoped
+public class HelloWorldManagedBean implements Serializable{
+
+    private static final long serialVersionUID = -6913972022251814607L;
+
+    private String name = "Ethien Salinas!!";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setS1(String name) {
+        this.name = name;
+    }  
+}
